dc5ac4d423df7716b80e0e2ab2b4c7c9786b1eaa
mitchplass <mitchellplass@gmail.com>
2026-09-11 10:38:41 +1000

    Initial Commit

---
+++ RideReady/RideReady/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RideReady
+//
+//  Created by Mitchell Plass on 11/9/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RideReady/RideReady/RideReadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideReadyApp.swift
+//  RideReady
+//
+//  Created by Mitchell Plass on 11/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct RideReadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
